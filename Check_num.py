d7bad9d29ede81c9ea20267e42d18ce7e91ca6f5
Dayananda  K S <dayanandaks045@gmail.com>
2025-09-14 22:24:10 +0530

    Create Check_num.py

---
+++ Check_num.py
@@ -0,0 +1,13 @@
+# Take a number and check if it is positive, negative, or zero.
+
+def check_num(num1):
+    if num1 > 0:
+        print(f"{num1} is Positive")
+    elif num1 < 0:
+        print(f"{num1} is Negative")
+    else:
+        print(f"{num1} is Zero")
+check_num(1) # 1 is Positive
+check_num(-9) # -9 is Negative
+check_num(0) # 0 is Zero
+
